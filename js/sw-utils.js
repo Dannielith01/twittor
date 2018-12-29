ad8a4854e33f914c4bd9ea6c4d9619388877fc4e
Daniel Zavala <daniel.zavalah@gmail.com>
2018-12-29 02:14:37 -0700

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+let updateDynamicCache = (dynamicCache, req, res) => {
+    if (res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res;
+        });
+    } else {
+        return res;
+    }
+};
